d7bd2985cd05a43cacf85d91f7383285fed81200
Akhmat Guliev <akhmat.guliev@stenn.com>
2023-08-05 23:21:24 +0200

    Solution added,And first project with Test Controller

---
+++ WebApp/Controller/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("OkiDoki");
+    }
+}
